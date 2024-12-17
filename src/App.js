53d038cde87907edcc9bd86bca11266be367556b
Devzeeshan5669 <devzeeshan1@gmail.com>
2024-12-17 12:16:58 +0500

    Fixing Issues & Wrapping up TextUtils

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-  const [Mode, setMode] = useState('light');
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
@@ -18,34 +22,35 @@ function App() {
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  };
+  }
 
   const toggleMode = () => {
-    if (Mode === 'light') {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", 'success');
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", 'success');
-
+      showAlert("Light mode has been enabled", "success");
     }
-  };
+  }
+
   return (
     <>
-    <Router>
-      <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="
-ENTER THE TEXT TO ANALYZE BELOW:-" mode={Mode} />} />
-        </Routes>
-      </div>
-    </Router>
-  </>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            {/* /users --> Component 1
+                /users/home --> Component 2 */}
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -94,7 +94,7 @@ const reverseText = () => {
         <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Minutes read</p>
         <h2>preview</h2>
-        <p>{text.length>0?text:"Enter something in the above textbox to preview it here"}</p>
+        <p>{text.length>0?text:"Nothing to preview! "}</p>
       </div>
     </>
   );
